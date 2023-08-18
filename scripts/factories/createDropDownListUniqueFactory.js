f2bbe4ec86963e5cdcf6dc6be1bdae7292b38f2c
mamadykonte <contactmamadykonte@gmail.com>
2023-08-18 18:48:03 +0200

    fin

---
+++ scripts/factories/createDropDownListUniqueFactory.js
@@ -0,0 +1,37 @@
+import { handleChangeData } from "../utils/filterData.js";
+
+const createDropDownListUniqueFactory = (
+  filterName,
+  { search, recipes, tags, refresh }
+) => {
+  const data = handleChangeData(search, recipes, tags, refresh);
+
+  const lists = data.map((recipe) => recipe[filterName]);
+
+  const flatList = lists.flat();
+
+  let result = [];
+
+  switch (filterName) {
+    case "ingredients":
+      result = [
+        ...new Set(flatList.map((ingredient) => ingredient.ingredient)),
+      ];
+      break;
+
+    case "appliance":
+      result = [...new Set(lists)];
+      break;
+
+    case "ustensils":
+      result = [...new Set(flatList)];
+      break;
+
+    default:
+      break;
+  }
+
+  return result.sort();
+};
+
+export default createDropDownListUniqueFactory;
